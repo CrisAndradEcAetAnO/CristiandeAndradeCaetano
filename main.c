e0cd59c3e0333edcfb11c5c4316522c452dfd430
CrisAndradEcAetAnO <101538159+CrisAndradEcAetAnO@users.noreply.github.com>
2022-03-24 22:11:05 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int produto, desconto;
+  printf("Informe o preco do produto: ");
+  scanf("%d",&produto);
+
+  desconto = 0.1;
+
+  printf("Preco com desconto: %d",desconto);
+}
+return 0;
+
